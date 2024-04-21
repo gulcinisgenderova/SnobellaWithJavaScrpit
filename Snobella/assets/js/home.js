640c0847dfe9ae7a52f04d4336bbe6ba59e07a24
vusalasalimovaa <salimovavusala7@gmail.com>
2024-04-21 15:02:47 +0400

    ok

--- Snobella/assets/js/home.js
+++ Snobella/assets/js/home.js
@@ -1,1 +1,14 @@
-const featuredPrd=document
+axios("https://fakestoreapi.com/products").then((res) => {
+  res.data.forEach((element) => {
+    const products = document.querySelector(".products");
+    const cardsDiv = document.createElement("div");
+    const imageProduct = document.createElement("img");
+    const titleProduct = document.createElement("p");
+    const priceProduct = document.createElement("p");
+    const raitingProduct = document.createElement("p");
+    const basketButton = document.createElement("button");
+    const favButton = document.createElement("button");
+
+
+  });
+});
