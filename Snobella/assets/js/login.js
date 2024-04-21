75ba7688eceffdcdc535c842a635034518d4d91b
gulcinisgenderova <mi7rtfggj@code.edu.az>
2024-04-21 14:00:28 +0400

    Second

--- Snobella/assets/js/login.js
+++ Snobella/assets/js/login.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
 {
     
 }
@@ -56,4 +56,4 @@ logForm.addEventListener("submit", (e) => {
      alert("Username ve passwordunuzu daxil edin!")
     }
   });
->>>>>>> fe333e220ff18d389cb962885f70cc39c5accb11
+// >>>>>>> fe333e220ff18d389cb962885f70cc39c5accb11
